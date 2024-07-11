7de36e2c02364e7f70db59ea182ab91b8243b02d
Alexandre Silva <alexandre260678@hotmail.com>
2024-07-11 09:37:30 -0300

    Final do Projeto WEBJS2

--- Projetos/WEBJS2/main.js
+++ Projetos/WEBJS2/main.js
@@ -1,6 +1,12 @@
-function tocaSom (idElementoAudio) {
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+    
+    if (elemento != null && elemento.localName === 'audio') {
+            elemento.play();
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido.');
+    }
 
-    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
